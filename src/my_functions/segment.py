33105e29ca5433a1cd4c4bf9d5dcc637f41c3c1f
jacopo <jacopo.lungo@hotmail.it>
2024-01-08 22:40:27 +0100

    added method to get road_masks

--- src/my_functions/segment.py
+++ src/my_functions/segment.py
@@ -381,7 +381,17 @@ def clean_mask(road_lines: Union[LineString, List[LineString]],
 #############
 
 
-
+def get_road_masks_b(query_bbox_b, proj_roads_gdf: gpd.GeoDataFrame, sample_size, dataset_res, ext_mt = 10):
+    for bbox in query_bbox_b:
+        query_bbox_poly = boundingBox_2_Polygon(bbox)
+        road_hits = proj_roads_gdf.geometry.intersects(query_bbox_poly)
+        if len(road_hits) != 0:
+            queried_proj_roads_gdf = proj_roads_gdf[road_hits]
+            road_lines = rel_road_lines(queried_proj_roads_gdf, query_bbox_poly, dataset_res)
+            buffered_lines = road_lines.buffer(ext_mt)
+            road_mask_b = rasterize(buffered_lines, out_shape=(1, sample_size, sample_size))
+        
+    
 
 #############
 #Trees
